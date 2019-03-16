c4c297bcb849b26364bf8dde38981cc18a354f8b
Leif Lislegard <lislei@kartverket.no>
2019-03-16 20:53:53 +0100

    Single line property values in i10n property files / resource bundles. + removed duplicate newlines in arabic and hebrew i10n files + regression tests + tests that demonstrates how to read bi-directional / RTL text

--- core/src.test/com/biglybt/core/internat/MessageTextTest.java
+++ core/src.test/com/biglybt/core/internat/MessageTextTest.java
@@ -1,5 +1,6 @@
 package com.biglybt.core.internat;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
@@ -53,5 +54,91 @@ public class MessageTextTest
 		assertThat(MessageText.getDefaultLocaleString("ConfigView.label.startminimized"))
 				.isEqualTo("Start minimized");
 	}
+
+	@Test
+	public void mixedTextDirections() {
+		MessageText.changeLocale(new Locale("ar", "SA"));
+
+		assertThat(MessageText.getString("iconBar.start.tooltip"))
+				.isEqualTo("\u0628\u062F\u0621 \u062A\u0634\u063A\u064A\u0644 torrent(s) \u0627\u0644\u0645\u062D\u062F\u062F");
+	}
+
+	@Test
+	public void mixedTextDirectionsAndSubstitutions() {
+		MessageText.changeLocale(new Locale("ar", "SA"));
+
+		assertThat(MessageText.getString("MainWindow.dialog.exitconfirmation.title"))
+				.isEqualTo("\u0625\u0646\u0647\u0627\u0621 BiglyBT");
+	}
+
+	/**
+	 * Internal string representation is stored in the same order you read the sentence (including arabic)
+	 * <blockquote>
+	 *   Java SE stores text in memory in logical order, which is the order in which characters and words are read and written.
+	 *   The logical order is not necessarily the same as the visual order, which is the order in which the corresponding glyphs are displayed.
+	 * </blockquote>
+	 * Source: https://docs.oracle.com/javase/tutorial/2d/text/textlayoutbidirectionaltext.html#ordering_text
+	 */
+	@Test
+	public void arabicTextIsStoredTheSameWayYouReadArabic() {
+		MessageText.changeLocale(new Locale("ar", "SA"));
+
+		String arabicString = MessageText.getString("MainWindow.dialog.exitconfirmation.title");
+		assertThat(arabicString).isEqualTo("\u0625\u0646\u0647\u0627\u0621 BiglyBT");
+
+		//utf16 for simplicity as most arabic characters falls in the extended two bytes range of utf8
+		byte[] internalBytes = arabicString.getBytes(StandardCharsets.UTF_16);
+		assertThat(internalBytes)
+				.inHexadecimal()
+				.containsExactly(
+						0xFE, 0xFF, //BOM (the utf16 conversion likes to add those two bytes)
+						0x06, 0x25,
+						0x06, 0x46,
+						0x06, 0x47,
+						0x06, 0x27,
+						0x06, 0x21,
+						0x00, (int) ' ', //0x20 (single bytes in utf8)
+						0x00, (int) 'B',
+						0x00, (int) 'i',
+						0x00, (int) 'g',
+						0x00, (int) 'l',
+						0x00, (int) 'y',
+						0x00, (int) 'B',
+						0x00, (int) 'T');
+	}
+
+	/**
+	 * Demonstration of use of line breaks and text flow.
+	 *
+	 * <blockquote>
+	 * <p>
+	 * Arabic, Hebrew and a few other languages run the inline direction from right to left.
+	 * This is commonly known as RTL.
+	 * <p>
+	 * Note that the inline direction still runs horizontally.
+	 * The block direction runs from top to bottom. And the characters are upright.
+	 * </blockquote>
+	 *
+	 * Source: https://24ways.org/2016/css-writing-modes/
+	 * <br>
+	 * <br>
+	 * Note: Visual text representation is not illustrated in this example.
+	 */
+	@Test
+	public void arabicTextLinesReadsFromTopToBottom() {
+		MessageText.changeLocale(new Locale("ar", "SA"));
+
+		String twoArabicLetters = "\u0625\u0646";
+		StringBuilder stringBuilder = new StringBuilder(twoArabicLetters);
+		stringBuilder.insert(1, '\n'); //inserting newline
+		String twoLinesOfArabicLetters = stringBuilder.toString();
+
+		assertThat(twoLinesOfArabicLetters)
+				.isEqualTo("\u0625\n\u0646");
+
+		assertThat(twoLinesOfArabicLetters.replace("\n", ""))
+				.isEqualTo(twoArabicLetters);
+	}
+
 
 }
