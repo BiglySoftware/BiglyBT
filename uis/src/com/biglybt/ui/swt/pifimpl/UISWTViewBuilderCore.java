293ab38d74729ab3db0c7de84c75f6d2fc8d1bac
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2019-10-07 15:03:38 -0700

    fix logic ! bit

--- uis/src/com/biglybt/ui/swt/pifimpl/UISWTViewBuilderCore.java
+++ uis/src/com/biglybt/ui/swt/pifimpl/UISWTViewBuilderCore.java
@@ -264,7 +264,7 @@ public class UISWTViewBuilderCore
 				Debug.out("Won't be able to create view " + viewID
 						+ " because listener class '" + cla.getName() + " is private");
 			}
-			if (Modifier.isStatic(modifiers)) {
+			if (!Modifier.isStatic(modifiers)) {
 				Debug.out("Won't be able to create view " + viewID
 					+ " because listener class '" + cla.getName() + " is not static");
 			}
