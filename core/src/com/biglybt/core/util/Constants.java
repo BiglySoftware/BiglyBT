94aaf9191f7dca850e9b73d24ef177285427d93d
parg <parg@users.noreply.github.com>
2019-12-15 16:27:58 +0000

    Prep 2203

--- core/src/com/biglybt/core/util/Constants.java
+++ core/src/com/biglybt/core/util/Constants.java
@@ -150,11 +150,11 @@ Constants
 	public static final String BIGLY_PEER_ID						= "BI";
 
 	
-	public static final String BIGLYBT_VERSION  = "2.2.0.2";
+	public static final String BIGLYBT_VERSION  = "2.2.0.3_CVS";
 	
 	//public static final String BUILD_VERSION  = "@build.version@";   //Ant replace - believed dead
 	public static final String SUBVERSION		= "";
-	public static final byte[] VERSION_ID       = ("-" + BIGLY_PEER_ID + "2202" + "-").getBytes();  //MUST be 8 chars long!
+	public static final byte[] VERSION_ID       = ("-" + BIGLY_PEER_ID + "2203" + "-").getBytes();  //MUST be 8 chars long!
 
 	private static final boolean FORCE_NON_CVS = System.getProperty( "az.force.noncvs", "0" ).equals( "1" );
 
