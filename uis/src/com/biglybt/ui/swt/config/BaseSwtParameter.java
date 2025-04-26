a11a911bbd110b9937d73c586c749954079b5328
parg <parg@users.noreply.github.com>
2025-04-26 13:55:44 +0100

    Tweak

--- uis/src/com/biglybt/ui/swt/config/BaseSwtParameter.java
+++ uis/src/com/biglybt/ui/swt/config/BaseSwtParameter.java
@@ -795,8 +795,9 @@ public abstract class BaseSwtParameter<PARAMTYPE extends SwtParameter<VALUETYPE>
 
 			}else{
 				
-				gc.setForeground(display.getSystemColor(enabled
-						? SWT.COLOR_WIDGET_FOREGROUND : SWT.COLOR_WIDGET_LIGHT_SHADOW));
+				gc.setForeground(
+					display.getSystemColor(
+						enabled?SWT.COLOR_WIDGET_FOREGROUND : (Utils.isDarkAppearanceNative()?SWT.COLOR_WIDGET_DISABLED_FOREGROUND:SWT.COLOR_WIDGET_LIGHT_SHADOW)));
 			}
 			
 			gc.drawLine(x, y, x, y2);
